2d214217c67d75b77ad67ffed0a69874a51f178b
Kunal <kunalshinde0612@gmail.com>
2024-08-06 11:11:03 +0530

    commit

--- Static and final/StaticBlock.java
+++ Static and final/StaticBlock.java
@@ -1,15 +1,28 @@
-class Practice{
-    static{
+class Practice {
+    // Constructor for the Practice class
+    public Practice() {
+        System.out.println("Hello");
+    }
+    
+    // Static block 1: Executes when the class is first loaded
+    static {
         System.out.println("Block 1");
     }
-    static{
+    
+    // Static block 2: Executes when the class is first loaded, after static block 1
+    static {
         System.out.println("Block 2");
     }
 }
-public class StaticBlock{
-    public static void main(String[] args)
-    {
+
+public class StaticBlock {
+    public static void main(String[] args) {
+        // Creating an instance of the Practice class
         Practice p = new Practice();
+        // This will call the constructor of Practice class, printing "Hello"
+        
+        // Print statement in the main method
         System.out.println("Main");
     }
 }
+
